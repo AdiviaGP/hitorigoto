fc613f8a89e6a31dda9594b93874cab9b39a95c8
Your Name <you@example.com>
2017-08-26 09:38:43 +0700

    Upload Repository

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TAUISLAM</title>
+    <link rel="stylesheet" href="/sass/style.css">
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+
+      </div>
+      <nav>
+
+      </nav>
+    </header>
+<!--########### COVER ############-->
+
+    <section class="cover">
+        <div class="tau">
+          <h1>TAUISLAM</h1>
+          <sub></sub>
+        </div>
+    </section>
+<!--########### MENU ############-->
+
+    <section class="menu">
+      <div class="box ceramah">CERAMAH</div>
+      <div class="box tanya">TANYA USTADZ</div>
+      <div class="box motivasi">MOTIVASI</div>
+      <div class="box cerita">CERITA MUALAF</div>
+    </section>
+  </body>
+</html>
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TAUISLAM</title>
+    <link rel="stylesheet" href="/sass/layout.css">
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+
+      </div>
+      <nav>
+
+      </nav>
+    </header>
+<!--########### header banner ############-->
+    <section class="banner">
+      <div class="judul">
+        <h1>CERAMAH</h1>
+      </div>
+    </section>
+
+<!--########### header banner ############-->
+    <section class=""></section>
+  </body>
+</html>
